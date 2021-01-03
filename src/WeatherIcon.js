2a64ee7575bddbaa58c1c4e12a8815909424274b
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-03 22:30:59 +0100

    Changed method weather icon
    The app is no longer broken but the description appears instead of the icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,42 +1,30 @@
 import React from "react";
-import clear-day from "./icons/clear-day.svg";
-import clear-night from "./icons/clear-night.svg";
-import cloudy from "./icons/cloudy.svg";
-import overcast from "./icons/overcast.svg";
-import rain from "./icons/rain.svg";
-import partly-cloudy-day-rain from "./icons/partly-cloudy-day-rain.svg";
-import partly-cloudy-night-rain from "./icons/partly-cloudy-night-rain.svg";
-import thunderstorms-day from "./icons/thunderstorms-day.svg";
-import thunderstorms-night from "./icons/thunderstorms-night.svg";
-import partly-cloudy-day-snow from "./icons/partly-cloudy-day-snow.svg";
-import partly-cloudy-night-snow from "./icons/partly-cloudy-night-snow.svg";
-import mist from "./icons/mist.svg";
 
-function WeatherIcon(props){
+function WeatherIcon({code, alt}){
       const codeMapping = {
-        "01d": "clear-day",
-        "01n": "clear-night",
-        "02d": "cloudy",
-        "02n": "cloudy",
-        "03d": "cloudy",
-        "03n": "cloudy",
-        "04d": "overcast",
-        "04n": "overcast",
-        "09d": "rain",
-        "09n": "rain",
-        "10d": "partly-cloudy-day-rain",
-        "10n": "partly-cloudy-night-rain",
-        "11d": "thunderstorms-day",
-        "11n": "thunderstorms-night",
-        "13d": "partly-cloudy-day-snow",
-        "13n": "partly-cloudy-night-snow",
-        "50d": "mist",
-        "50n": "mist",
+        "01d": "./icons/clear-day.svg",
+        "01n": "./icons/clear-night.svg",
+        "02d": "./icons/cloudy.svg",
+        "02n": "./icons/cloudy.svg",
+        "03d": "./icons/cloudy.svg",
+        "03n": "./icons/cloudy.svg",
+        "04d": "./icons/overcast.svg",
+        "04n": "./icons/overcast.svg",
+        "09d": "./icons/rain.svg",
+        "09n": "./icons/rain.svg",
+        "10d": "./icons/partly-cloudy-day-rain.svg",
+        "10n": "./icons/partly-cloudy-night-rain.svg",
+        "11d": "./icons/thunderstorms-day.svg",
+        "11n": "./icons/thunderstorms-night.svg",
+        "13d": "./icons/partly-cloudy-day-snow.svg",
+        "13n": "./icons/partly-cloudy-night-snow.svg",
+        "50d": "./icons/mist.svg",
+        "50n": "./icons/mist.svg",
       }
 
       return(
         <div className="weatherIcons">
-          <img src={codeMapping[props.code]} alt={alt} className="weatherIcon" />
+          <img src={codeMapping[code]} alt={alt} className="weathericon" />
         </div>
       );
 }
