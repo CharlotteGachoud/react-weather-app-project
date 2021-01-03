73f3c28f02144538a5e0d0cda8fb2ce37ca85a69
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-03 22:17:30 +0100

    Modified icons names
    still message error
    
    ./src/WeatherIcon.js
    SyntaxError: C:\Users\chaga\Desktop\Coding\react-weather-app-project\src\WeatherIcon.js: Unexpected token (2:12)
    
      1 | import React from "react";
    > 2 | import clear-day from "./icons/clear-day.svg";
        |             ^
      3 | import clear-night from "./icons/clear-night.svg";
      4 | import cloudy from "./icons/cloudy.svg";
      5 | import overcast from "./icons/overcast.svg";

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,21 +1,21 @@
 import React from "react";
-import clearDay from "./icons/clearDay.svg";
-import clearNight from "./icons/clearNight.svg";
+import clear-day from "./icons/clear-day.svg";
+import clear-night from "./icons/clear-night.svg";
 import cloudy from "./icons/cloudy.svg";
 import overcast from "./icons/overcast.svg";
 import rain from "./icons/rain.svg";
-import partlyCloudyDayRain from "./icons/partlyCloudyDayRain.svg";
-import partlyCloudyNightRain from "./icons/partlyCloudyNightRain.svg";
-import thunderstormsDay from "./icons/thunderstormsDay.svg";
-import thunderstormsNight from "./icons/thunderstormsNight.svg";
-import partlyCloudyDaySnow from "./icons/partlyCloudyDaySnow.svg";
-import partlyCloudyNightSnow from "./icons/partlyCloudyNightSnow.svg";
+import partly-cloudy-day-rain from "./icons/partly-cloudy-day-rain.svg";
+import partly-cloudy-night-rain from "./icons/partly-cloudy-night-rain.svg";
+import thunderstorms-day from "./icons/thunderstorms-day.svg";
+import thunderstorms-night from "./icons/thunderstorms-night.svg";
+import partly-cloudy-day-snow from "./icons/partly-cloudy-day-snow.svg";
+import partly-cloudy-night-snow from "./icons/partly-cloudy-night-snow.svg";
 import mist from "./icons/mist.svg";
 
 function WeatherIcon(props){
       const codeMapping = {
-        "01d": "clearDay",
-        "01n": "clearNight",
+        "01d": "clear-day",
+        "01n": "clear-night",
         "02d": "cloudy",
         "02n": "cloudy",
         "03d": "cloudy",
@@ -24,19 +24,19 @@ function WeatherIcon(props){
         "04n": "overcast",
         "09d": "rain",
         "09n": "rain",
-        "10d": "partlyCloudyDayRain",
-        "10n": "partlyCloudyNightRain",
-        "11d": "thunderstormsDay",
-        "11n": "thunderstormsNight",
-        "13d": "partlyCloudyDaySnow",
-        "13n": "partlyCloudyNightSnow",
+        "10d": "partly-cloudy-day-rain",
+        "10n": "partly-cloudy-night-rain",
+        "11d": "thunderstorms-day",
+        "11n": "thunderstorms-night",
+        "13d": "partly-cloudy-day-snow",
+        "13n": "partly-cloudy-night-snow",
         "50d": "mist",
         "50n": "mist",
       }
 
       return(
         <div className="weatherIcons">
-          <img src={codeMapping[props.code]} alt={alt}/>
+          <img src={codeMapping[props.code]} alt={alt} className="weatherIcon" />
         </div>
       );
 }
