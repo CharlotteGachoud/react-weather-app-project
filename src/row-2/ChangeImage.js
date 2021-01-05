0e771914c0c2a8e6e36651cf1ec05a6c3dda6a62
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-06 00:15:36 +0100

    Changed images to gifs

--- src/row-2/ChangeImage.js
+++ src/row-2/ChangeImage.js
@@ -4,20 +4,26 @@ function ChangeImage(props){
 
   if (props.temperature >= 20 ){
   return(
-      <img src="Images/Pool.svg" className="illustration" alt="summer"/>
+    <div className="illustration">
+      <iframe title="jaws" src="https://giphy.com/embed/5pYdt8lWiM2qzyXuVS" width="300" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thecoolidge-jaws-coolidge-corner-theatre-5pYdt8lWiM2qzyXuVS"></a></p>
+    </div>
   );
   } else if(props.temperature >= 15 ){
     return(
-     <img src="Images/Blooming.svg" className="illustration" alt="spring"/>
+    <div className="illustration">
+      <iframe title="alergies" src="https://giphy.com/embed/l2JehsPxGvhrsRuBq" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/season-8-the-simpsons-8x2-l2JehsPxGvhrsRuBq"></a></p>
+    </div>
     );
   } else if(props.temperature >= 10 ){
     return(
-     <img src="Images/Fall_is_coming.svg" className="illustration" alt="autumn"/>
-    );
+    <div className="illustration">
+      <iframe title="Leaf" src="https://giphy.com/embed/xFnAObsU5nMrUUC3HX" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><a href="https://giphy.com/gifs/cute-aww-eyebleach-xFnAObsU5nMrUUC3HX" />
+    </div>    );
   }else {
     return(
-     <img src="Images/Winter_magic.svg" className="illustration" alt="winter"/>
-    );
+    <div className="illustration">
+      <iframe title="Titanic" src="https://giphy.com/embed/grO6JXGhRUFBeRGKFA" width="300" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/titanic-jackandrose-titanicdoor-grO6JXGhRUFBeRGKFA"></a></p>
+    </div>    );
   }
 }
 
