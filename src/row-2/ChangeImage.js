a05504f89657e9716ae9d0b5ebf07828244be6d7
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-08 21:00:31 +0100

    Added a gif and changed sentences

--- src/row-2/ChangeImage.js
+++ src/row-2/ChangeImage.js
@@ -7,7 +7,11 @@ function ChangeImage(props){
     <div className="jaws">
       <iframe title="jaws" src="https://giphy.com/embed/tNaQg4kIR8dOw" width="400" height="204" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/challenger-halloween-challenger23-jaws-tNaQg4kIR8dOw">via GIPHY</a></p>
       <p className="sentence">
-        Watch out for Bruce if you go for a swim 🦈
+        “Just when you thought it was safe
+        <br/>
+        <strong> to go back in the water...</strong>” 🦈
+        <br/>
+        <small>― Peter Benchley, Jaws</small>
       </p>
     </div>
   );
@@ -16,7 +20,15 @@ function ChangeImage(props){
     <div className="illustration">
       <iframe title="alergies" src="https://giphy.com/embed/l2JehsPxGvhrsRuBq" width="350" height="350" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/season-8-the-simpsons-8x2-l2JehsPxGvhrsRuBq">via GIPHY</a></p>
       <p className="sentence">
-        Yay it's allergies season again!!!
+        With just one glimps
+        <br/>
+        you make my eyes water.
+        <br/>
+        Caused not by your beauty
+        <br/>
+        but by your tingling yellow powder.
+        <br/>
+        <strong>Thank F#@k I'm not a bee!</strong>
       </p>
     </div>
     );
@@ -25,16 +37,34 @@ function ChangeImage(props){
     <div className="leaves">
       <iframe title="Leaf" src="https://giphy.com/embed/xFnAObsU5nMrUUC3HX" width="400" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cute-aww-eyebleach-xFnAObsU5nMrUUC3HX">via GIPHY</a></p>
       <p className="sentence">
-        Oh my god! Are these..? ...LEAVES 🤪
+        Oh my god! Are these..? <strong>...LEAVES</strong> 🤪
       </p>
     </div>
     );
+  }else if(props.temperature >= 0){
+      return(
+        <div className="wakeMeUp">
+          <iframe title="cozy blanket" src="https://giphy.com/embed/7n8CrwJfRhBe44K3K8" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/spring-warm-cozyblanket-7n8CrwJfRhBe44K3K8">via GIPHY</a></p>
+          <p className="sentence">
+            Come to the dark side!
+            <br />
+              We have blankets...{" "}
+            <strong>
+              and cookies 🍪
+              </strong>
+          </p>
+        </div>
+      );
   }else {
     return(
     <div className="titanic">
       <iframe title="Titanic" src="https://giphy.com/embed/grO6JXGhRUFBeRGKFA" width="400" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/titanic-jackandrose-titanicdoor-grO6JXGhRUFBeRGKFA">via GIPHY</a></p>
       <p className="sentence">
-        Aaaah Come on! <br/>There was enough space on that door... 
+        Aaaah Come on! <br/>
+        <strong>
+          There was enough space{" "} 
+          </strong>
+          on that door... 
       </p>
     </div>
     );
