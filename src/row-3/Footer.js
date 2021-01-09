a52d3243e7ce93aa1d8cc6b6a59b16b2667eda45
CharlotteGachoud <73673161+CharlotteGachoud@users.noreply.github.com>
2021-01-09 15:35:12 +0100

    Updated Footer

--- src/row-3/Footer.js
+++ src/row-3/Footer.js
@@ -1,17 +1,22 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer(){
   return(
     <div className="Footer">
-      <div className="code-by">
+      <span className="code-by">
         <a href="https://github.com/CharlotteGachoud/react-weather-app-project" className="open-source-link" target="_blank" rel="noreferrer">
           Open-source code
         </a>{" "}
         by{" "}
-        <a href="https://www.linkedin.com/in/charlotte-gachoud-5210811b9/" target="_blank" rel="noreferrer">
-          Charlotte Gachoud
-        </a>
-      </div>
+        <a href="https://www.linkedin.com/in/charlotte-gachoud-5210811b9/" target="_blank" rel="noreferrer" className="author-link">
+            Charlotte Gachoud
+        </a> | {" "}
+      </span>
+        <span className="icon-credit">
+        <a href="https://www.amcharts.com/free-animated-svg-weather-icons/" target="_blank" rel="noreferrer" className="icon-link">Weather icons</a> by 
+          amCharts
+        </span>
     </div>
   )
 }
